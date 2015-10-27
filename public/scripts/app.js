69155d889eb4a6206307c2dcf070d4e9578dafcc
Blessan Mathew <blessenm@gmail.com>
2015-10-28 00:26:18 +0530

    Now author names and tweet id are correctly fetched if the tweet is a retweet.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,11 +11,20 @@ function getProfile() {
 function getTweets() {
   fetchData('tweets', function(tweets) {
     tweets = tweets.map(function(tweet) {
-      var profileImage = tweet.user.profile_image_url;
-      var profileName = tweet.user.name + '(@' + tweet.user.screen_name + ')';
+      if(tweet.retweeted_status) {
+        var tweetId = tweet.retweeted_status.id_str;
+        var username = tweet.retweeted_status.user.screen_name;
+        var profileImage = tweet.retweeted_status.user.profile_image_url;
+        var profileName = tweet.retweeted_status.user.name + '(@' + tweet.retweeted_status.user.screen_name + ')';
+      } else {
+        var tweetId = tweet.id_str;
+        var username = tweet.user.screen_name;
+        var profileImage = tweet.user.profile_image_url;
+        var profileName = tweet.user.name + '(@' + tweet.user.screen_name + ')';
+      }
       var tweetMarkup = tweet.html;
 
-      return `<li class="brm-list-item">
+      return `<li class="brm-list-item" data-id="${tweetId}" data-username="${username}">
 
         <div class="bookmark"></div>
 
@@ -31,9 +40,9 @@ function getTweets() {
 
     document.querySelector('.bmr-tweet-list').innerHTML = tweets.join('');
     document.querySelector('.uil-squares-css').hidden = true;
+
+    setupEventListeners();
   });
-
-
 }
 
 function fetchData(url, callback) {
@@ -46,6 +55,26 @@ function fetchData(url, callback) {
   .then(callback)
   .catch(function(err) {
     console.log(err);
+  });
+}
+
+function setupEventListeners() {
+  var list = document.querySelector('.bmr-tweet-list');
+  var iframe = document.querySelector('.bmr-tweet-page');
+
+  list.addEventListener('click', function(event) {
+    var target = event.target; // Clicked element
+    while (target && target.parentNode !== list) {
+        target = target.parentNode; // If the clicked element isn't a direct child
+        if(!target) { return; } // If element doesn't exist
+    }
+    //console.log(target);
+    if (target.tagName === 'LI'){
+        var id = target.getAttribute('data-id');
+        var username = target.getAttribute('data-username');
+        //iframe.setAttribute('src', `https://twitter.com/${username}/status/${id}`); //Cant be done to frameblocker
+        window.open(`https://twitter.com/${username}/status/${id}`, '_blank');
+    }
   });
 }
 
