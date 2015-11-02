9003fe3c60fae5afac1c662cb203c0404f61ff43
Blessan Mathew <blessenm@gmail.com>
2015-11-02 07:37:57 +0530

    Removed debugger statement. Rearranged some code. Trying to style modules with the order - requires, exports, module variable declarations/initializations, functions

--- app_modules/db-manager.js
+++ app_modules/db-manager.js
@@ -2,19 +2,20 @@ var lowDb = require('lowdb');
 var path = require('path');
 var merge = require('lodash.merge');
 
+module.exports.getUser = getUser;
+module.exports.saveUser = saveUser;
+
 var databasePath = './database';
 
 var usersDb = lowDb(path.join(databasePath, 'users.json'));
 usersDb = usersDb('users');
 
-module.exports.getUser = getUser;
-module.exports.saveUser = saveUser;
 
 function getUser(username) {
   return usersDb.find({username: username});
 }
 
-function saveUser(userData) {debugger;
+function saveUser(userData) {
   var newUserObj = {syncCount: 0};
 
   var userObj = usersDb.find({username: userData.username});
