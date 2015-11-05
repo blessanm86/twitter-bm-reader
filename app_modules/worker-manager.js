d66e8f86c5400699c691f0c7ea3521b9015cd59e
Blessan Mathew <blessenm@gmail.com>
2015-11-06 00:28:17 +0530

    WIP. Making the worker-manager.js a singleton.

--- app_modules/worker-manager.js
+++ app_modules/worker-manager.js
@@ -1,49 +1,99 @@
 var fork = require('child_process').fork;
 
-module.exports.addWork = addWork;
+// module.exports.addWork = addWork;
 
-var workerQueue = [];
-var worker;
-var isWorking = false;
-var callbacks = [];
+// var workerQueue = [];
+// var worker;
+// var isWorking = false;
+// var callbacks = [];
 
-//setup
-createObserverForQueue();
-createAWorker();
+// //setup
+// createObserverForQueue();
+// createAWorker();
 
-function addWork(work, callback) {
-  callback = callback || function() {};
-  callbacks.push(callback);
-  workerQueue.push(work);
+// function addWork(work, callback) {
+//   callback = callback || function() {};
+//   callbacks.push(callback);
+//   workerQueue.push(work);
+// }
+
+// function createObserverForQueue() {
+//   Array.observe(workerQueue, function(changes) {
+//     if(changes[0].addedCount && !isWorking) {
+//       work();
+//     }
+//   });
+// }
+
+// function createAWorker() {
+//   worker = fork('./app_modules/worker.js');
+
+//   worker.on('message', function done(message) {
+//     isWorking = false;
+//     callbacks.shift()(message);
+
+//     if(workerQueue.length) {
+//       work();
+//     }
+//   });
+// }
+
+// function work() {
+//   var workName = workerQueue.shift();
+//   isWorking = true;
+
+//   worker.send(workName);
+// }
+
+function QueueManager() {console.log('QueueManager init');
+  var workerQueue = [];
+  var worker;
+  var isWorking = false;
+  var callbacks = [];
+
+  function createObserverForQueue() {
+    Array.observe(workerQueue, function(changes) {
+      if(changes[0].addedCount && !isWorking) {
+        work();
+      }
+    });
+  }
+
+  function createAWorker() {
+    worker = fork('./app_modules/worker.js');
+
+    // worker.on('message', function done(message) {
+    //   isWorking = false;
+    //   callbacks.shift()(message);
+
+    //   if(workerQueue.length) {
+    //     work();
+    //   }
+    // });
+  }
+
+  function work() {
+    var workName = workerQueue.shift();
+    isWorking = true;
+
+    worker.send(workName);
+  }
+
+  createObserverForQueue();
+  createAWorker();
+
+  this.addWork = function(work, callback) {
+    callback = callback || function() {};
+    callbacks.push(callback);
+    workerQueue.push(work);
+  }
 }
 
-function createObserverForQueue() {
-  Array.observe(workerQueue, function(changes) {
-    if(changes[0].addedCount && !isWorking) {
-      work();
-    }
-  });
-}
+module.exports = exports = new QueueManager();
 
-function createAWorker() {
-  worker = fork('./app_modules/worker.js');
-
-  worker.on('message', function done(message) {
-    isWorking = false;
-    callbacks.shift()(message);
-
-    if(workerQueue.length) {
-      work();
-    }
-  });
-}
-
-function work() {
-  var workName = workerQueue.shift();
-  isWorking = true;
-
-  worker.send(workName);
-}
+// module.exports = exports = function() {
+//   console.log('wtf');
+// }();
 
 
 
