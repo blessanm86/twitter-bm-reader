b6c82b55c741c3fc10108e64d9ea7165bd00189a
Blessan Mathew <blessenm@gmail.com>
2015-11-09 22:10:37 +0530

    Used the pauseable package to pause the sync timer on errors.

--- app_modules/user-sync.js
+++ app_modules/user-sync.js
@@ -1,18 +1,14 @@
+var pauseable = require('pauseable');
 var workerManager = require('./worker-manager');
 
 module.exports = function() {
-  function startSyncing() {
-    var syncTimerId = setInterval(function() {
-      console.log('Scheduling sync');
+  var syncInterval = pauseable.setInterval(1000 * 10, function() {
+    console.log('Scheduling sync');
 
-      workerManager.addWork({jobName: 'syncUsers'}, function(didSync) {
-        if(!didSync) {
-          clearInterval(syncTimerId);
-          setTimeout(startSyncing, 1000 * 30);
-        }
-      });
-    }, 1000 * 10);
-  }
-
-  startSyncing();
+    workerManager.addWork({jobName: 'syncUsers'}, function(didSync) {
+      if(!didSync) {
+        syncInterval.pause(1000 * 30);
+      }
+    });
+  });
 }
